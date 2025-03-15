342d6c8948c30cc42c06b77caef6ab55ec632756
Marvita Ferreira <m4rvita@gmail.com>
2025-03-15 14:49:17 -0300

    atualização: usando SDL3 e imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,71 @@
-#include "pomodoro.h"
+#include <SDL3/SDL.h>
+#include "imgui.h"
+#include "backends/imgui_impl_sdl3.h"
+#include "backends/imgui_impl_sdlrenderer3.h"
 #include <iostream>
 
-using namespace std;  // Adicionando isso para evitar o uso de std:: em todo o código
-
 int main() {
-    int workMinutes;
-
-    cout << "Digite o tempo de trabalho (minutos): ";
-    cin >> workMinutes;
-
-    if (workMinutes <= 0) {
-        cout << "O tempo de trabalho deve ser maior que zero. Tente novamente.\n";
-        return 1;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+        SDL_Log("Erro ao inicializar SDL: %s", SDL_GetError());
+        return -1;
     }
 
-    Pomodoro pomodoro(workMinutes * 60);  // Convertendo para segundos
-    pomodoro.startSession();
+    SDL_Window *window = SDL_CreateWindow("Teste SDL3 + ImGui", 800, 600, 0);
+    if (!window) {
+        SDL_Log("Erro ao criar a janela: %s", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL, 0);
+    if (!renderer) {
+        SDL_Log("Erro ao criar o renderizador: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Inicializa ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO(); (void)io;
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if (event.type == SDL_EVENT_QUIT) running = false;
+        }
+
+        // Inicia o frame ImGui
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // Interface Gráfica
+        ImGui::Begin("Teste ImGui");
+        ImGui::Text("Hello, ImGui com SDL3!");
+        if (ImGui::Button("Sair")) running = false;
+        ImGui::End();
+
+        // Renderiza
+        ImGui::Render();
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData());
+        SDL_RenderPresent(renderer);
+    }
+
+    // Cleanup
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
